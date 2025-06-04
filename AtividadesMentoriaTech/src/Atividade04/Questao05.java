cda5375c03a5b5556a1d9a05e7fcd2696c7f9b6f
Maria Aparecida <maria.cavalcante.2008@aluno.edu.pi.gov.br>
2025-06-04 10:12:38 -0300

    Meu segundo codigo java

---
+++ AtividadesMentoriaTech/src/Atividade04/Questao05.java
@@ -0,0 +1,13 @@
+package Atividade04;
+
+public class Questao05 {
+    public static void main(String[] args) {
+        int idade1 = 20;
+        int idade2 = 13;
+
+        boolean resultado = idade1 > idade2;
+
+        System.out.println("A primeira pessoa e a mais velha: "+ resultado);
+
+    }
+}
