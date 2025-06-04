7694182f80ad1020fc77071c24e9a470480b70cf
Maria Aparecida <maria.cavalcante.2008@aluno.edu.pi.gov.br>
2025-06-04 09:46:19 -0300

    Meu primeiro codigo java

---
+++ AtividadesMentoriaTech/src/Atividade04/Questao04.java
@@ -0,0 +1,13 @@
+package Atividade04;
+
+public class Questao04 {
+    public static void main(String[] args) {
+        double base = 6.0;
+        double altura = 10.0;
+
+        double area_retangulo = base * altura;
+        double perimetro_retangulo = 2 * (base + altura);
+        System.out.println("area do retangulo: " + area_retangulo);
+        System.out.println("perimetro do retangulo: " + perimetro_retangulo);
+    }
+}
